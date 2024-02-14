f71f3fcc6aa1ae8283713a3c0ac56160c56e2bd8
Sahar <sahar.mahmoudi67@gmail.com>
2024-02-14 10:15:42 +0100

    Creat new file and folder for data analysis. Change the name of main to scrape.py

---
+++ scrape.py
@@ -0,0 +1,29 @@
+from scraper.immoscraper import ImmoCrawler
+import asyncio
+import sys
+import time
+import threading
+
+def spinner():
+    while True:
+        for cursor in '|/-\\':
+            sys.stdout.write(cursor)
+            sys.stdout.flush()
+            time.sleep(0.1)
+            sys.stdout.write('\b')
+
+
+
+async def main():
+    print("\nImmoCrawler is running...", end="", flush=True)
+    threading.Thread(target=spinner, daemon=True).start()  # Start spinner in a separate thread
+    crawler = ImmoCrawler()
+    await crawler.get_properties(num_pages=333)
+    crawler.to_csv("dayta")
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
+    
+    
